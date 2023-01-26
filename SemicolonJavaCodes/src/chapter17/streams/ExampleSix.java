f025a53ec5c9b681141667d7296ea69ac08f1cac
Martins-O <jojololamartins686@gmail.com>
2023-01-27 00:50:06 +0100

    it been awhile

--- SemicolonJavaCodes/src/chapter17/streams/ExampleSix.java
+++ SemicolonJavaCodes/src/chapter17/streams/ExampleSix.java
@@ -6,6 +6,6 @@ public class ExampleSix {
     public static void main(String[] args) {
         List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        System.out.println(nums.stream().count());
+        System.out.println((long) nums.size());
     }
 }
